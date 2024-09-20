c660be3416156b99ab0e17aab517c33139488744
adole aljasm <zoomby.2000@hotmail.com>
2024-09-20 14:21:36 +0300

    before update the map

--- obj/Release/net8.0/PandaGameLibrary.AssemblyInfo.cs
+++ obj/Release/net8.0/PandaGameLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PandaGameLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba6893610f628933a01889326998687430bc6f23")]
 [assembly: System.Reflection.AssemblyProductAttribute("PandaGameLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PandaGameLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
